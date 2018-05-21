50e3b548bbc2714456645105e9a3c3ce4ce56eea
anton.bilchuk <anton.bilchuk@stylelounge.com>
2018-05-21 17:36:29 +0300

    feature: unzip folder to /outputs folder

--- src/app.js
+++ src/app.js
@@ -3,12 +3,17 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const { checkToken } = require('./middleware')
-// const fs = require('fs')
+const fs = require('fs')
 const multer = require('multer')
+const unzip = require('unzip')
+const AdmZip = require('adm-zip')
+
+const fileOutputPath = path.join(__dirname, '../output')
+const fileInputPath = path.join(__dirname, '../inputs/')
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, '../', '/files/'))
+    cb(null, fileInputPath)
   },
   filename: function (req, file, cb) {
     const project = req.body.project
@@ -21,6 +26,12 @@ const upload = multer({ storage })
 const PORT = process.env.PORT
 
 app.post('/project_name/', upload.single('zip'), checkToken, (req, res) => {
+  const file = req.file
+
+  const zip = new AdmZip(file.path)
+
+  zip.extractAllTo(fileOutputPath, true)
+
   res.send({
     success: true
   })
